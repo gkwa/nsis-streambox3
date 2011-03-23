536dde1488dbc137b98a0dbbe53a5730edf4e8a8
boxstream <taylor.monacelli@streambox.com>
2011-03-22 17:36:35 -0700

    Fix NSIS_Simple_Service_Plugin setup batch file

--- nsis-plugins/NSIS_Simple_Service_Plugin_1.28.zip-setup.bat
+++ nsis-plugins/NSIS_Simple_Service_Plugin_1.28.zip-setup.bat
@@ -1,10 +1,10 @@
 REM -*- bat -*-
 @Echo on
 
-set stage=NSIS_Simple_Service_Plugin_1.28.zip
+set stage=NSIS_Simple_Service_Plugin_1.28-odXihBhRVpA4
 mkdir "%stage%"
 .\unzip -q -n NSIS_Simple_Service_Plugin_1.28.zip -d "%stage%"
 
-copy /y "%stage%\SimpleFC.dll" "%programfiles%\NSIS\Unicode\Plugins"
+copy /y "%stage%\SimpleSC.dll" "%programfiles%\NSIS\Unicode\Plugins"
 
 rmdir /q/s "%stage%"
