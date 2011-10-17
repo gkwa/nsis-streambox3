2ac42a79283ef73fb121e42057981ba5e6e7f5d4
boxstream <taylor.monacelli@streambox.com>
2011-10-16 19:14:59 -0700

    Upgrade NsProcess.zip to nsProcess_1_6.7z to support Unicode
    Change-Id: Ia2e15fab0b4e6b157a05df33da8ae9726fa8a5b9

--- nsis-plugins/NsProcess.zip-setup.bat
+++ nsis-plugins/NsProcess.zip-setup.bat
@@ -1,13 +1,12 @@
 REM -*- bat -*-
 @Echo on
 
-set plugin=NsProcess
-set zip=%plugin%.zip
+set plugin=nsProcess_1_6
+set zip=%plugin%.7z
 set stage=%cd%\%zip%-odXihBhRVpA4
 set r=%cd%\..\robocopy.exe
 
-mkdir "%stage%"
-.\unzip -q -n "%zip%" -d "%stage%"
+.\7za x -y "%zip%" -o"%stage%"
 
 cd "%programfiles%\NSIS"
 
@@ -15,3 +14,34 @@ if exist Plugins\nul ( %r% "%stage%" "%programfiles%\NSIS" /e /s )
 if exist Unicode\nul ( %r% "%stage%" "%programfiles%\NSIS\Unicode" /e /s )
 
 rmdir /q/s "%stage%"
+
+
+
+REM [edit] Links
+
+REM Download v1.5:
+REM nsProcess.zip (25 KB)
+
+REM Download v1.6 (NSIS UNICODE support, by brainsucker):
+REM nsProcess_1_6.7z
+
+REM Discussion:
+REM Forum thread
+REM [edit] Description
+
+REM Features:
+
+REM     Find a process by name
+REM     Kill all processes with specified name (not only one)
+REM     Ð¡lose all processes with specified name (first tries to close all process windows, waits for 3 seconds for process to exit, terminates if still alive, use _CloseProcess function)
+REM     The process name is case-insensitive
+REM     Win95/98/ME/NT/2000/XP/Win7 support
+REM     Finds processes of other user(s) when running 'as Administrator' or when having switched to another user
+REM     Small plugin size (4 Kb)
+REM     NSIS UNICODE support (just rename nsProcessW.dll into nsProcess.dll) 
+
+REM [edit] Thanks
+
+REM Ravi Kochhar (source function FIND_PROC_BY_NAME based upon his code)
+REM iceman_k (Find_Process_By_Name) and DITMan (KillProcDLL_Manual) for direct me
+REM Category: Plugins
