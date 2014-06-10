d13a4fba6aea34f9e9a0f9d1af7289fa2e8e47b0
taylormonacelli <tailor@u.washington.edu>
2014-06-09 22:52:17 -0700

    Add curl.exe and download NSIS and tools

--- setup.bat
+++ setup.bat
@@ -1,6 +1,13 @@
 @Echo off
 REM -*- bat -*-
 
+curl -O http://installer-bin.streambox.com/7za.exe
+curl -O http://installer-bin.streambox.com/autoit-v3-setup.exe
+curl -O http://installer-bin.streambox.com/nsis-2.46-Unicode-setup.exe
+curl -O http://installer-bin.streambox.com/nsis-2.46-setup.exe
+curl -O http://installer-bin.streambox.com/sed-4.2-1-dep.zip
+curl -O http://installer-bin.streambox.com/sed-4.2-1-bin.zip
+
 :: usage cmd /c setup.bat
 
 if not exist "%programfiles%\NSIS\NUL" ( 
